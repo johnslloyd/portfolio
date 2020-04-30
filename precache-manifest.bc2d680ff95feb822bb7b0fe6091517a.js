172e00882edc2f5e2a6ecab9bfafaad4b0966bb2
johnslloyd <63673548+johnslloyd@users.noreply.github.com>
2020-04-30 14:06:36 -0500

    Updates

---
+++ precache-manifest.bc2d680ff95feb822bb7b0fe6091517a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a02cfa1e5e9925a7b1be33d8a75b25c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6155893cd718e6ecd2eb",
+    "url": "/portfolio/static/css/main.4a763fac.chunk.css"
+  },
+  {
+    "revision": "8df960779721a3184acc",
+    "url": "/portfolio/static/js/2.83a79dc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.83a79dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6155893cd718e6ecd2eb",
+    "url": "/portfolio/static/js/main.f7b5121f.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "dbd85e858a59a5608dc07f8456aea4a7",
+    "url": "/portfolio/static/media/Dreamhome-mobileweb-property.dbd85e85.png"
+  },
+  {
+    "revision": "a9c79262950f243d917e97801373f5fb",
+    "url": "/portfolio/static/media/Dreamhome-wires.a9c79262.png"
+  },
+  {
+    "revision": "27c16cda93e1f95d476d26d3899aa184",
+    "url": "/portfolio/static/media/beverlyhilton-web-600.27c16cda.png"
+  },
+  {
+    "revision": "e1f5f1a8a82151353fc27328f60e1a2e",
+    "url": "/portfolio/static/media/dreamhome-agile.e1f5f1a8.png"
+  },
+  {
+    "revision": "5a18446b62d03f1e7f7fcb725cada239",
+    "url": "/portfolio/static/media/dreamhome-workshop.5a18446b.png"
+  },
+  {
+    "revision": "66c5f27bfbc5c7fa80b25d25754a2718",
+    "url": "/portfolio/static/media/dreamhome.66c5f27b.png"
+  },
+  {
+    "revision": "0f10a46db648ef0d5c5aca5825f05bb7",
+    "url": "/portfolio/static/media/fractal-web-600.0f10a46d.png"
+  },
+  {
+    "revision": "df711b3101a29012575ad23756280f13",
+    "url": "/portfolio/static/media/heroes-home-web.df711b31.png"
+  },
+  {
+    "revision": "4bba1bfd5e6805cec80db91f5f64c634",
+    "url": "/portfolio/static/media/hilotn-iphone-600.4bba1bfd.png"
+  },
+  {
+    "revision": "a2d847df1706494ba0ddd4e2d7768500",
+    "url": "/portfolio/static/media/hilotn-web-600.a2d847df.png"
+  },
+  {
+    "revision": "3617c870c680ffc13348ace05d3f5a8e",
+    "url": "/portfolio/static/media/hilton-connectedroom-600.3617c870.png"
+  },
+  {
+    "revision": "64be95329726ef2775f2aa07daaeb4a4",
+    "url": "/portfolio/static/media/peer-to-peer-workshop.64be9532.png"
+  },
+  {
+    "revision": "242d8e66e25c1e205e5b83196f585d06",
+    "url": "/portfolio/static/media/pep-ipad-600.242d8e66.png"
+  },
+  {
+    "revision": "f186bb0aa816e0ba2f75e0f6ffc3ed66",
+    "url": "/portfolio/static/media/stjude-dreamhome-600.f186bb0a.png"
+  },
+  {
+    "revision": "b39ee1d3ba03403613842fbdc3ab4006",
+    "url": "/portfolio/static/media/stjude-heroes-600.b39ee1d3.png"
+  },
+  {
+    "revision": "6578f4c8bf03f0962d177fee9c4dd2b7",
+    "url": "/portfolio/static/media/stjude-org-600.6578f4c8.png"
+  },
+  {
+    "revision": "654dd7892b1b3643f25bd72458fbd076",
+    "url": "/portfolio/static/media/stjudeweb-1800.654dd789.png"
+  },
+  {
+    "revision": "1a2b84bc03b0b33c1ee651a8ee5c4cac",
+    "url": "/portfolio/static/media/walk-home-mobileweb.1a2b84bc.png"
+  }
+]);
